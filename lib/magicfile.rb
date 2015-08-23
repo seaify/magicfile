0e56666ae0f0ac019740ca845461903882651eae
seaify <dilin.life@gmail.com>
2015-08-23 18:05:23 +0800

    update

--- lib/magicfile.rb
+++ lib/magicfile.rb
@@ -3,11 +3,10 @@ require 'active_support'
 require 'active_support/core_ext/string'
 
 class MagicLine
-  attr_accessor :content, :parent_line_index, :space_num
+  attr_accessor :content, :space_num
 
-  def initialize(content='', parent_line_index=-1, space_num=0)
+  def initialize(content='', space_num=0)
     @content = content
-    @parent_line_index = parent_line_index
     @space_num = space_num
   end
 end
@@ -20,12 +19,38 @@ class Magicfile
     @current_line_space_num = current_line_space_num
   end
 
+  def append_class(name, parent_class_name=nil)
+
+    if parent_class_name
+      class_start_line = MagicLine.new("class #{name.camelize} < #{parent_class_name}")
+    else
+      class_start_line = MagicLine.new("class #{name.camelize}")
+    end
+
+    append_after_current(class_start_line, true, 2)
+    append_after_current(MagicLine.new("end"), false, 0)
+  end
+
   def append_module(name)
     module_start_line = MagicLine.new("module #{name.camelize}")
     module_end_line = MagicLine.new("end")
 
     append_after_current(module_start_line, true, 2)
     append_after_current(module_end_line, false, 0)
+  end
+
+  def append_modules(names)
+    names.each do |name|
+      append_module(name)
+    end
+  end
+
+  def append_string_lines(line_contents)
+    space_num = @current_line_space_num + 2
+    content = line_contents.join("\n" + " " * space_num)
+    ap content
+    line = MagicLine.new(content, space_num)
+    @lines.insert(@current_line_index + 1, line)
   end
 
   def append_after_current(line, enable_move_down=true, spaces=2)
@@ -48,6 +73,12 @@ class Magicfile
 
   end
 
+  def to_file(name)
+    infos = @lines.map { |line| " " * line.instance_variable_get("@space_num") + line.instance_variable_get("@content") }
+    ap infos.join("\n")
+    File.open(name, 'w') { |file| file.write(infos.join("\n")) }
+  end
+
   def display
     @lines.each do |line|
       ap ' ' * line.instance_variable_get("@space_num") + line.instance_variable_get("@content")
@@ -57,11 +88,7 @@ class Magicfile
 end
 
 a = Magicfile.new
-ap a
-a.append_module('api')
-a.display
-
-a.append_module('v1')
-a.display
-
-ap a.instance_variable_get("@lines")
+a.append_modules(['api', 'v1', 'entities'])
+a.append_class('screen', 'Grape::Entity')
+a.append_string_lines(['expose :id', 'expose :name'])
+a.to_file('tmp.txt')
--- magicfile.gemspec
+++ magicfile.gemspec
@@ -1,11 +1,11 @@
 Gem::Specification.new do |s|
   s.name        = "magicfile"
-  s.version     = "0.0.1"
-  s.date        = "2015-08-22"
-  s.summary     = "sum"
-  s.description = "des"
-  s.authors     = ["sea"]
-  s.email       = "dil"
+  s.version     = "0.0.2"
+  s.date        = "2015-08-23"
+  s.summary     = "used to create file, modify file, read file information"
+  s.description = "used to create file, modify file, read file information"
+  s.authors     = ["seaify"]
+  s.email       = "dilin.life@gmail.com"
   s.files       = Dir["lib/magicfile.rb"]
   s.homepage    = "https://github.com/seaify/magicfile"
   s.license     = "MIT"
